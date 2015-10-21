af1976d771dabfac053da37e0e3ef4d6786df676
unknown <aron.hjartarson@WS-083.assentis.info>
2015-10-21 15:09:22 +0200

    First commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+(function() {
+	var $section = $('section').first();
+	$section.find('.panzoom').panzoom({
+		$zoomIn: $section.find(".zoom-in"),
+		$zoomOut: $section.find(".zoom-out"),
+		$zoomRange: $section.find(".zoom-range"),
+		$reset: $section.find(".reset")
+	});
+})();
